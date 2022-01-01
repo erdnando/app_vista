d1a421cc548b00a8a2ffdbdba05c41c8d3da80b1
erdnando <erdnando@gmail.com>
2022-01-01 12:54:18 -0600

    chart do párecer

--- src/components/ultimasActualizaciones/HeaderActualizaciones.tsx
+++ src/components/ultimasActualizaciones/HeaderActualizaciones.tsx
@@ -11,7 +11,7 @@ export const HeaderActualizaciones = ( { label }: Props ) => {
 
     return (
       <View style={{ width:'90%',alignContent:'flex-start', left:0,top:95+30,}}>
-          <Text style={{ fontFamily:'Roboto-Bold', fontSize:20}}>{label}</Text>
+          <Text style={{ fontFamily:'Roboto-Bold', fontSize:17}}>{label}</Text>
           <Spacer height={20}></Spacer>
       </View>
     )
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -10,6 +10,7 @@ import { Search } from '../../components/search/Search';
 import { ResumenOportunidades } from '../../components/resumen/ResumenOportunidades';
 import { HeaderActualizaciones } from '../../components/ultimasActualizaciones/HeaderActualizaciones';
 import { ListActualizaciones } from '../../components/ultimasActualizaciones/ListActualizaciones';
+import { BarChart } from "react-native-gifted-charts";
 
 export const HomeScreen = () => {
     const { top } = useSafeAreaInsets();
@@ -35,8 +36,17 @@ export const HomeScreen = () => {
             )
      }else if(tipoUsuario === 2) {//colaborador
 
+        const labelTopGo = () =>{  return( <Text style={{width:70, textAlign:'right'}}>455</Text> ) }
+        const labelTopNoGo = () =>{  return( <Text style={{width:70, textAlign:'right'}}>210</Text> ) }
+        const labelEnAnalisis = () =>{  return( <Text style={{width:70, textAlign:'right'}}>6</Text> ) }
+        
 
-       
+        const barData = [
+            {value: 455,label: 'GO',frontColor: '#83AE69',topLabelComponent:labelTopGo },
+            {value: 210,label: 'NO GO',frontColor: '#B85050',topLabelComponent:labelTopNoGo },
+            {value: 6,label: 'EM ANALISE',frontColor: '#F9A61A',topLabelComponent:labelEnAnalisis },
+           
+            ];
 
 
         return (
@@ -51,14 +61,62 @@ export const HomeScreen = () => {
                             <Spacer height={8}></Spacer>
                            
                             {/* ultimas actualizaciones */}
-                            <View style={{flex:1, flexDirection:'column',backgroundColor:'#BCC1CB', position:'absolute', bottom:0, 
+                            {/* <View style={{flex:1, flexDirection:'column',backgroundColor:'#BCC1CB', position:'absolute', bottom:0, 
                                            width:'100%', height:Platform.OS=='ios' ? '73%': '78%',  alignItems:'center',}}>
                                 <HeaderActualizaciones label='Ultimas actualizaciones'></HeaderActualizaciones>
                                 <View style={{height:Platform.OS=='ios' ? '63%': '72%' }}>
                                     <ListActualizaciones></ListActualizaciones>
                                 </View>
-                               
+                                </View>
+                            </View> */}
+
+
+                            {/* Charts */}
+                            <View style={{flex:1, flexDirection:'column',backgroundColor:'#BCC1CB', position:'absolute', bottom:0, 
+                                           width:'100%', height:Platform.OS=='ios' ? '73%': '78%',  alignItems:'center',}}>
+                                <HeaderActualizaciones label='Resultado do parecer'></HeaderActualizaciones>
+
+                                <View style={{height:Platform.OS=='ios' ? '58%': '72%',bottom: -115, flexDirection:'column', width:'90%', left:-1, justifyContent:'center', 
+                                            alignItems:'flex-start', borderWidth: 0,backgroundColor:'white', borderRadius:7,padding:5,elevation:6,
+                                            shadowColor: "#000000", shadowOpacity: 0.4,shadowOffset: {
+                                            height: 1,
+                                            width: 1}}}>         
+                                 <BarChart
+                                 barWidth={70}
+                                 barBorderRadius={4}
+                                 verticalLinesThickness={1}
+                                 cappedBars={true}
+                                 capThickness={3}
+                                 capColor={'black'}
+                                 
+                                    showFractionalValues={true}
+                                    showLine={true}
+                                    showXAxisIndices={true}
+                                    showReferenceLine1={true}
+                                    showVerticalLines={true}
+                                    showYAxisIndices={true}
+                                    noOfSections={5}
+                                    maxValue={500}
+                                    data={barData}
+                                    isAnimated
+                                    
+                                    />
+                                
+                                </View>
+                                <HeaderActualizaciones label='Total de Pareceres: 710'></HeaderActualizaciones>
+                                {/* <View style={{height:Platform.OS=='ios' ? '63%': '72%', bottom:-150,backgroundColor:'white' }}>
+                                    <BarChart 
+                                    showFractionalValues={true}
+                                    showYAxisIndices
+                                    noOfSections={4}
+                                    maxValue={400}
+                                    data={barData}
+                                    isAnimated
+                                    />
+                                </View> */}
                             </View>
+
+
 
                             {/* Resumen de oportunidades */}
                             <ResumenOportunidades></ResumenOportunidades>
